b41615ae8446d66acd26a6f797e788ea3287a951
Hamish Macpherson <hamstu@gmail.com>
2013-04-26 11:43:35 -0700

    Fixed bug where paths with spaces would prevent jump

--- jump/jump.sh
+++ jump/jump.sh
@@ -17,5 +17,5 @@ jump.py $1                    # 2: Call our jump program (it outputs to the .jum
 newdir=`cat ~/.jumpfile`      # 3: Get the output of our .jumpfile into a variable
 
 if [[ X"" != X"$newdir" ]]    # 4: Jump if it's a non-empty string
-  then cd `echo $newdir`;echo $newdir
+  then cd "`echo $newdir`";echo $newdir
 fi
